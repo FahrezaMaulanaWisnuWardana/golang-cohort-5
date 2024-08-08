2a168268e98199920c560f49222211ac0a646120
Fahreza Maulana Wisnu Wardana <reza11.6a@gmail.com>
2024-08-09 00:37:37 +0700

    perubahan peletakan printLn

--- sesi-satu/main.go
+++ sesi-satu/main.go
@@ -6,11 +6,11 @@ func main() {
 	n := 45
 	for i := 1; i <= n; i++ {
 		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("Fizz")
-		} else if i%3 == 0 {
-			fmt.Println("Buzz")
-		} else if i%5 == 0 {
 			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
 		} else {
 			fmt.Println(i)
 		}
